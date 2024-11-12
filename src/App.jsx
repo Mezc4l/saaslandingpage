076ab922926dcb9fe8717176fa052f9e2f2f8791
unknown <mezcalonly@gmail.com>
2024-11-12 17:41:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Header, Hero, Features, Pricing, Faq, Testimonials, Download, Footer } from "./sections";
+
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Header from './Header'
+import Hero from './Hero'
+import Features from './Features'
+import Pricing from './Pricing'
+import Faq from './Faq'
+import Testimonials from './Testimonials'
+import Download from './Download'
+import Footer from './Footer';
+
+export {
+    Header,
+    Hero,
+    Features,
+    Pricing,
+    Faq,
+    Testimonials,
+    Download,
+    Footer,
+}
